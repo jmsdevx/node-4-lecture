d92552dbcbfae6d692651d05ae1d9c761ab4ad95
Jonathan May <jonathan.may@devmountain.com>
2019-02-04 09:48:15 -0600

    Add session

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require("express");
 const app = express();
 const PORT = 5050;
 const massive = require("massive");
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: false
+  })
+);
 
 massive(process.env.CONNECTION_STRING).then(dbInstance => {
   dbInstance.query(
